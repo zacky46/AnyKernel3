cb48a3a95a41571d297f907bf90a060cde4a6226
zacky <132923924+zacky46@users.noreply.github.com>
2023-05-27 07:49:36 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=MRT-ginkgo Kernel by zacky_ma @ Telegram
+kernel.string=HaruanGaming v6969696969 Kernel by zacky_ma @ Telegram
 do.devicecheck=1
 do.modules=0
 do.systemless=1
